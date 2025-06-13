7aac5e4a92179716ca935111f31cce6ffa4ca98f
Sameh Hamdalla <sa_mh87@outlook.de>
2025-06-13 22:24:44 +0200

    Erster Commit

---
+++ Javascript/main.js
@@ -0,0 +1,151 @@
+
+
+// let x = "   Ahmed                    ";
+// // 'let y = "ali";'
+// // console.log(x==y)
+
+// console.log(x.length)
+// console.log(x.trim().length)
+
+// let askemail = prompt("Please enter your mail");
+// let email = "sa_mh87@outlook.de"
+// console.log(askemail.toLowerCase().trim()==email);
+
+// // == values
+// console.log(5=="5")
+// // values and Data types
+// console.log(5==="5")
+// console.log(4 >= 5)
+// console.log(4 >= 4)
+// console.log(4 != 4)
+
+// and &&, or ||
+// let product = "TV-Samsung";
+// let size = 50;
+// let price = 10000;
+
+// // console.log(price >= 10000);
+
+// console.log( price < 12000 && size == 50 )
+// console.log( price < 12000 || size == 40 )
+
+// let num1 = 5;
+// let num2 = 10;
+
+// if (num2 > num1) 
+// { 
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// console.log("Hello");
+// }
+
+// if (num1 > num2) 
+// { 
+// console.log("Hello");
+// }
+
+// #######################################################
+
+// let num1 = prompt("num1")
+// let num2 = prompt("num2")
+// if (num2 > num1) {
+//     console.log( "Hallo Habibi") 
+// }
+
+// #######################################################
+
+// let role = prompt("Whats your role in the company?")
+
+// if (role == "admin"){
+//     document.write(" you can update, create and delete.")   
+// }
+
+// else if (role == "moderator") {
+//     document.write("you can just update and create.")   
+// }
+// else{
+//     document.write("Hallo User.")
+// }
+
+// #######################################################
+
+// let result = prompt("Whats is your result?")
+// result = parseFloat(result);
+
+// if (result >= 90) {                 
+//     document.write("Excellent");
+// }
+// //  oder "80%" "70%" "60%" "500%"
+// else if (result >= 80) {
+//     document.write("very good");
+// }
+// else if (result >= 70) {
+//     document.write("good");    
+// }
+
+// else if (result >= 50) {
+//     document.write("acceptable");  
+// }
+// else {
+//     document.write("fail");
+// }
+// #######################################################
+
+// age = prompt("What is your age ?")
+
+// age >=18?
+//     document.write("Hallo user")
+//     : document.write("no you are very young.")
+
+// #######################################################
+
+// let role = prompt("What is your role ?")
+
+// switch (role) {
+//     case "admin":
+//         document.write("create, update, delete");
+//         break;
+
+//     case "moderator" :
+//         document.write("create, update");
+//         break;
+
+//     case "editor":
+//         document.write("update");
+//         break;
+
+//     default:
+//         document.write("Hallo user")
+// }
+// #######################################################
+
+// let day = prompt("What day is it?");
+
+// switch (day.toLowerCase()) {
+//   case "monday":
+//     document.write("Start of the week");
+//     break;
+
+//   case "friday":
+//     document.write("Weekend is coming!");
+//     break;
+
+//   case "saturday":
+//   case "sunday":
+//     document.write("Weekend!");
+//     break;
+
+//   default:
+//     document.write("Just another day");
+// }
+// #######################################################
+
+
+
+
+
+
+
